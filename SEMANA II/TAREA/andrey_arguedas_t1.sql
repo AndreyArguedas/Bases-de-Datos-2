770305df51bd38044df61aa3eae47fc9487b5440
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2018-08-02 14:32:48 -0600

    Script tarea 1 terminada

--- SEMANA II/TAREA/andrey_arguedas_t1.sql
+++ SEMANA II/TAREA/andrey_arguedas_t1.sql
@@ -3,7 +3,7 @@
 host cls
 
 PROMPT ANDREY ARGUEDAS / BD2
-PROMPT Tarea  1 (3pm) 
+PROMPT Tarea #1 (3pm) 
 PROMPT Fecha Entrega: 06-agosto-2018
 
 PROMPT ==========================================
@@ -68,3 +68,98 @@ create role R_CONSULTA;
 create role R_ACTUALIZA;
 
 PROMPT ==========================================
+PROMPT Crear tabla empleado (codigo, nombre, salario) y la PK 
+PROMPT ambos en TABLESPACE SYSTEM
+
+create table empleado(codigo number not null primary key, nombre varchar2(10), salario number(5))
+tablespace SYSTEM;
+
+PROMPT ==========================================
+PROMPT Crear funcion fun_cant_emp
+
+create or replace function fun_cant_emp return number is
+  VCant number;
+begin
+  select count(*) into VCant from empleado;
+  return(VCant);
+end fun_cant_emp;
+/
+
+PROMPT ==========================================
+PROMPT Crear procedimiento prc_act_salario
+
+create or replace procedure prc_act_salario(PCod in number, PSalario in number) is
+
+begin
+  update empleado set salario = PSalario where codigo = PCod;
+  commit;
+end prc_act_salario;
+/
+
+PROMPT ==========================================
+PROMPT Crear vista REP_ALTOS (salarios altos)
+
+create or replace view REP_ALTOS as
+select nombre, salario from empleado where salario > 1000;
+
+PROMPT ==========================================
+PROMPT Insert de los 3 empleados directos
+
+insert into empleado (codigo, nombre, salario) values (1,'Juan', 1000);
+insert into empleado (codigo, nombre, salario) values (2,'Ana', 2000);
+insert into empleado (codigo, nombre, salario) values (3,'Pedro', 3000);
+commit;
+
+PROMPT ==========================================
+PROMPT Prueba de la funcion cantidad de empleados (ahora son 3)
+
+select fun_cant_emp() cantidad from dual;
+
+PROMPT ==========================================
+PROMPT Permisos 3 a R_CONSULTA // 2 a R_ACTUALIZA
+
+grant select on empleado to R_CONSULTA;
+grant execute on fun_cant_emp to R_CONSULTA;
+grant select on REP_ALTOS to R_CONSULTA;
+
+grant execute on prc_act_salario to R_ACTUALIZA;
+grant select on empleado to R_ACTUALIZA;
+
+PROMPT ==========================================
+PROMPT Dar rol a juan y a pedro
+
+grant R_ACTUALIZA to pedro;
+grant R_CONSULTA to juan;
+
+PROMPT ==========================================
+PROMPT Se conecta con juan hace pruebas
+
+conn juan/juan123
+
+select * from conta.empleado;	
+select conta.fun_cant_emp() cantidad from dual;
+select * from conta.REP_ALTOS;
+
+PROMPT ==========================================
+PROMPT Se conecta con Pedro,
+
+conn pedro/pedro123
+PROMPT ==========================================
+PROMPT error esperado, no se puede actualizar directamente solo por medio del procedimiento o
+
+update conta.empleado set salario = 1500 where codigo = 1;
+
+commit;
+PROMPT ==========================================
+PROMPT Si funciona la ejecucion del procedimiento y cambia el salario de Juan a 1500
+
+exec conta.prc_act_salario(1, 1500);
+PROMPT ==========================================
+PROMPT Select de la tabla funciona y salario actualizado
+
+select * from conta.empleado;
+PROMPT ==========================================
+PROMPT Fin de la Tarea #1
+PROMPT Andrey Arguedas
+
+
