2aea463e094f1e9b0eb4c95135205a85282dd96b
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2018-08-12 15:07:53 -0600

    Practicas

--- SEMANA III/PracticasFinDeSemana/ContinuacionLeccion5.sql
+++ SEMANA III/PracticasFinDeSemana/ContinuacionLeccion5.sql
@@ -1,11 +1,11 @@
 -- Basado en: start 2018-07-26-permisos8pm4.sql
--- start 2018-08-09-PrePracticaRoles#1.sql
+-- start ContinuacionLeccion5.sql
 host cls
 PROMPT Version 2
 PROMPT Nos conectamos como system
 conn system/root
 
-spool 2018-08-09-PrePracticaRoles#1.log
+spool ContinuacionLeccion5.log
 
 --CREATE TABLESPACE datos 
 --datafile 'C:\oraclexe\app\oracle\oradata\XE\datos.DBF'
@@ -22,6 +22,8 @@ drop user juan cascade;
 DROP ROLE R_CREA_TABLA;
 
 DROP ROLE R_CONSULTA;
+
+DROP ROLE R_PROCESOS;
 
 PROMPT Creamos el usuario CONTA (Sistema de Contabilidad)
 
@@ -78,8 +80,45 @@ begin
 end PRC_INS_EMP;
 /
 
+--6.2
+PROMPT 6.2
+-- ACTUALIZAR Usa el Codigo para actualizar el NOMBRE
+create or replace procedure Actualizar(PCod in number, PNombre in varchar2) is
+begin
+  update empleado set nombre = PNombre where codigo = PCod;
+	commit;
+end Actualizar;
+/
+
+-- BORRAR. recibe por parametro el ID y borra el registro
+
+create or replace procedure Borrar(PCod in number) is
+begin
+  delete empleado where codigo = PCod;
+	commit;
+end Borrar;
+/
+
+PROMPT 6.3
+
+--6.3 Usuario CONTA debe crear el rol R_PROCESOS
+
+create role R_PROCESOS;
+
+grant execute on Actualizar to R_PROCESOS;
+grant execute on Borrar to R_PROCESOS;
+
+PROMPT 6.4
+-- CONTA debe asignar el rol R_CONSULTA al rol R_PROCESOS
+grant R_CONSULTA to R_PROCESOS;
+
+
 insert into empleado (codigo,nombre) values (1,'Juan');
 commit;
+
+exec conta.Actualizar(1, 'Memingo');
+exec conta.Borrar(1);
+exec conta.PRC_INS_EMP(1, 'Juancito');
 
 --5.4
 PROMPT 5.4
