d27d266a878460298eb6b901e3121fc1ef666b9d
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2018-08-12 21:52:18 -0600

    Pre Practica terminada

--- SEMANA III/PracticasFinDeSemana/PrePracticaFinSemana.sql
+++ SEMANA III/PracticasFinDeSemana/PrePracticaFinSemana.sql
@@ -20,8 +20,24 @@ conn system/root
 PROMPT #3.1
 
 drop user proyecto cascade;
+drop user boby cascade;
+drop user peter cascade;
+drop user tina cascade;
+drop user grace cascade;
 
 create user proyecto identified by p123;
+
+PROMPT 6.1
+create user boby  identified by b123;
+create user peter identified by p123;
+create user tina  identified by t123;
+create user grace identified by g123;
+
+PROMPT 6.2
+grant create session to boby;
+grant create session to peter;
+grant create session to tina;
+grant create session to grace;
 
 PROMPT #3.2
 
@@ -30,6 +46,7 @@ DROP ROLE R_CON1;
 DROP ROLE R_ACT1;
 DROP ROLE R_INS1;
 DROP ROLE R_BOR1;
+DROP ROLE R_ADM1;
 
 CREATE ROLE R_PROYECTO;
 
@@ -58,6 +75,7 @@ CREATE ROLE R_CON1;
 CREATE ROLE R_ACT1;
 CREATE ROLE R_INS1;
 CREATE ROLE R_BOR1;
+CREATE ROLE R_ADM1;
 
 PROMPT #4.2
 
@@ -105,6 +123,56 @@ grant execute on INSERTAR to R_INS1;
 grant execute on Borrar to R_BOR1;
 
 PROMPT 5.2
+grant R_CON1 to R_ADM1;
+grant R_ACT1 to R_ADM1;
+grant R_INS1 to R_ADM1;
+grant R_BOR1 to R_ADM1;
+
+PROMPT 7.1
+grant R_CON1 to boby;
+
+PROMPT 7.2
+grant R_CON1 to peter;
+grant R_INS1 to peter;
+
+PROMPT 7.3 
+grant R_CON1 to tina;
+grant R_INS1 to tina;
+grant R_ACT1 to tina;
+
+PROMPT 7.4)
+grant R_ADM1 to grace;
+
+PROMPT 8.1
+conn peter/p123
+execute proyecto.INSERTAR(1,'Tibas',1500);
+select * from proyecto.BODEGAS;
+
+PROMPT 8.2
+conn boby/b123
+execute proyecto.INSERTAR(2,'Uruca',2500);
+--Genera un "error esperado"
+select * from proyecto.BODEGAS;
+
+PROMPT 8.3
+conn tina/t123
+execute proyecto.INSERTAR(3,'Llorente',3500);
+select * from proyecto.BODEGAS;
+execute proyecto.Actualizar(3,'LLORENTE',4500);
+select * from proyecto.BODEGAS;
+execute proyecto.Borrar(3);
+select * from proyecto.BODEGAS;
+
+--Nota: En borrar se presenta un "error esperado", pues tina no tiene ese permiso!
+
+PROMPT 8.4
+conn grace/g123
+execute proyecto.INSERTAR(4,'Liberia',8500);
+select * from proyecto.BODEGAS;
+execute proyecto.Actualizar(3,'LIBERIA',9000);
+select * from proyecto.BODEGAS;
+execute proyecto.Borrar(3);
+select * from proyecto.BODEGAS;
 
 spool off
 PROMPT ======================================
