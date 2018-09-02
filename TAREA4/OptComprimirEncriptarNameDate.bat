95b9de2170a23d6cdf5e3f9578c358e48a829834
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2018-09-01 19:41:24 -0600

    Tarea4 final

--- TAREA4/OptComprimirEncriptarNameDate.bat
+++ TAREA4/OptComprimirEncriptarNameDate.bat
@@ -13,18 +13,19 @@ set /p Var4=
 REM valor posicional 25 al 100
 SET VVar1=%Var1:~25,100%
 SET VVar2=%Var2:~25,100%
-SET VVar3=%Var3:~25,100%
-SET VVar4=%Var4:~25,100%
+SET VVar3=%Var3:~28,100%
+SET VVar4=%Var4:~28,100%
+
 
 C:\TAREA4\7z.exe a %VVar3%.7z C:\TAREA4\OptUsuario.html C:\TAREA4\OptReporteObjUsuario.xml
 
-C:\TAREA4\aescrypt.exe -e -p clave123 %VVar3%.7z
+C:\TAREA4\aescrypt.exe -e -p %VVar4% %VVar3%.7z
 
 C:\TAREA4\namedate.exe /Y /ZZ:"F(Y-m-d)-(H-M-S).X" C:\TAREA4\%VVar3%.7z.aes
 
 del C:\TAREA4\OptUsuario.html
 del C:\TAREA4\OptReporteObjUsuario.xml
-del C:\TAREA4\tarea4.7z
+del C:\TAREA4\%VVar3%.7z
 
 dir /b *.aes > comprimido.ini
 
