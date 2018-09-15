33652e2b2a1dda4c2918048f711f4c7a77261585
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2018-09-14 21:00:40 -0600

    Parte 1 Proyecto

---
+++ Proyecto/MenuRespaldos.bat
@@ -0,0 +1,31 @@
+@ECHO off
+cls
+:start
+cls
+ECHO. --Proyecto #1---
+ECHO. --Andrey Arguedas Espinoza---
+ECHO. --MENU RESPALDOS---
+ECHO 1. Respaldo Usuario Oracle
+
+ECHO 0. Salir
+
+set choice=
+set /p choice=Indique la tarea correspondiente:
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto OptRespaldoUsuario
+
+if '%choice%'=='3' goto OptSalir
+ECHO "%choice%" Opcion incorrecta.
+pause
+ECHO.
+goto start
+
+:OptRespaldoUsuario
+start %~dp0\OptRespaldoUsuario.bat
+exit
+
+:OptSalir
+start %~dp0\controles.bat
+exit
+
+:end
