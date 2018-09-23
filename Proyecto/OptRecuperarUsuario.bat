a746c6ffa15fbe3f92e63e37d8002b0448f2882d
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2018-09-23 11:06:27 -0600

    A punto de terminar

--- Proyecto/OptRecuperarUsuario.bat
+++ Proyecto/OptRecuperarUsuario.bat
@@ -22,10 +22,10 @@ SET VVar1=%Var1:~0,100%
 
 REM Desencripta
 
-aescrypt.exe -d -p clave123 -o ./datos/%Respaldo%.7z ./respaldos/%VVar1%
+aescrypt.exe -d -p clave123 -o ./datos/%Respaldo%.7z ./respaldos/%VVar1% 1> ./logs/desencripta.log 2> ./logs/desencripta.err
 
 REM Descomprime en la carpeta actual
-7z.exe x ./datos/%Respaldo%.7z
+7z.exe x ./datos/%Respaldo%.7z 1> ./logs/descomprime.log 2> ./logs/descomprime.err
 
 move %Respaldo%.dmp ./datos/
 
@@ -39,8 +39,40 @@ echo touser=%User% >> ./parametros/importar.par
 REM Importa el respaldo al usuario deseado
 imp parfile=./parametros/importar.par
 
-pause
+echo Inicio================================================ >> ./logs/imports.log
+echo #1 Crear el usuario %User% en la base de datos         >> ./logs/imports.log
+type .\logs\BorrarCrear.log                                 >> ./logs/imports.log
+
+echo ______________________________________________________ >> ./logs/imports.log
+echo #2 Se desencripta el respaldo (.aes) en un 7z    		>> ./logs/imports.log
+echo error:                                                 >> ./logs/imports.log
+type .\logs\desencripta.err                                 >> ./logs/imports.log
+echo log:                                                   >> ./logs/imports.log
+type .\logs\desencripta.log                                 >> ./logs/imports.log
+
+echo ______________________________________________________ >> ./logs/imports.log
+echo #3 Se descomprime el respaldo (7z)    					>> ./logs/imports.log
+echo error:                                                 >> ./logs/imports.log
+type .\logs\descomprime.err                                 >> ./logs/imports.log
+echo log:                                                   >> ./logs/imports.log
+type .\logs\descomprime.log                                 >> ./logs/imports.log
+
+echo ______________________________________________________ >> ./logs/imports.log
+echo #4 Se realiza el import hacia el usuario %User%   	    >> ./logs/imports.log
+echo error:                                                 >> ./logs/imports.log
+type .\logs\importar.err                                    >> ./logs/imports.log
+echo log:                                                   >> ./logs/imports.log
+type .\logs\importar.log                                    >> ./logs/imports.log
+
+echo ______________________________________________________ >> ./logs/imports.log
+echo Final ================================================ >> ./logs/imports.log
 
 del recuperado.ini
+del .\logs\BorrarCrear.log
+del .\logs\desencripta.err
+del .\logs\desencripta.log
+del .\logs\descomprime.err
+del .\logs\descomprime.log
+del .\logs\importar.log
 
 exit
